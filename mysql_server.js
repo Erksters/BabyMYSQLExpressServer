ca3bdb8f0d2cd6424637db38e03c2b6bee787834
Erick <erksters@gmail.com>
2021-09-07 00:07:18 -0500

    initial commit. Works well, needs more urls

---
+++ mysql_server.js
@@ -0,0 +1,61 @@
+const mysql = require("mysql");
+const express = require('express');
+const formidable = require('express-formidable');
+
+var app = express();
+
+app.use(formidable());
+// Creating connection
+let db_con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "ROYGBIabc123.",
+  database: "all_baby_names"
+});
+
+// Connect to MySQL server
+db_con.connect((err) => {
+  if (err) {
+    console.log("Database Connection Failed !!!", err);
+  } else {
+    console.log("connected to Database");
+  }
+});
+
+app.post('/', (req, res) => {
+  res.send(JSON.stringify({ "status": 200 })  // <==== req.body will be a parsed JSON object  
+  )
+})
+
+app.post('/get_total_count_by_name', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  console.log("counting...", req.fields.username)
+  var query1 = `Select name, SUM(count) as total
+               from baby_names 
+               where name ="${req.fields.username}"
+               group by name;
+               `
+  db_con.query(query1, (err, rows) => {
+    if (err) throw err;
+    res.send(rows);  // <==== req.body will be a parsed JSON object
+  });
+})
+
+app.post('/get_total_count_by_name_and_year', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/Show_total_count_by_name");
+  console.log("counting...")
+  var query1 = `Select name, SUM(count) as total
+               from baby_names 
+               where name ="${req.fields.username}"" and birth_year = "${req.fields.birth_year}"
+               group by name;
+               `
+  db_con.query(query1, (err, rows) => {
+    if (err) throw err;
+    res.send(rows);  // <==== req.body will be a parsed JSON object
+  });
+})
+
+// Server setup
+app.listen(5000, () => {
+  console.log('server listening on port 5000');
+});
