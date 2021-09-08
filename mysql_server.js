622d05e9921ba4eb3557cbdee7cebd47c8cc6001
Erick <erksters@gmail.com>
2021-09-08 18:25:29 -0500

    added many years sql

--- mysql_server.js
+++ mysql_server.js
@@ -93,6 +93,30 @@ app.post('/get_total_count_by_name_and_state', (req, res) => {
   });
 })
 
+
+app.post('/get_many_counts_by_year', (req, res) => {
+  var query3 = `SELECT birth_year, count(count)
+   FROM baby_names 
+   where name = "${req.fields.username}"
+    group by birth_year order by birth_year asc;`
+
+  // Connect to MySQL server
+  db_con.getConnection((err, conn) => {
+    if (err) {
+      console.log("Database Connection Failed !!!", err);
+    } else {
+      console.log("connected to Database");
+      console.log(`counting... ${req.fields.username} over the years`)
+      conn.query(query3, (err2, rows) => {
+        if (err2) { console.log(err2); res.send(JSON.stringify({ "Status": "500" })) }
+        else { res.send(rows); }  // <==== req.body will be a parsed JSON object
+      });
+      conn.release()
+    }
+  });
+})
+
+
 // Server setup
 app.listen(process.env.PORT || 3000, () => {
   console.log(`server listening on port 3000`);
